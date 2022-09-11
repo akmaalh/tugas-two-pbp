9432776ca4b26af6ae449a6f44dee925ee68c49e
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-09-11 17:43:04 +0700

    completing code

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_katalog,
+        'name': 'Muhammad Akmal Hakim',
+        'id': '2106750383',
+    }
+    return render(request, "katalog.html", context)
